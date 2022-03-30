eda2f02435e8a6cceddaf940efb800023ec71e57
skyecas12 <101337163+skyecas12@users.noreply.github.com>
2022-03-30 13:20:18 +0100

    Add files via upload
    BASE finish Assign1 OOP

---
+++ Assignment1OOP/Assignment1 OOP VS 2019/Analyse.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_OOP_VS_2019
+{
+    public class Analyse
+    {
+        //Handles the analysis of text
+
+        //Method: analyseText
+        //Arguments: string
+        //Returns: list of integers
+        //Calculates and returns an analysis of the text
+        char[] Not_VowelLower = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'y', 'v', 'x', 'z', 'w' };
+        char[] Is_VowelLower = { 'a', 'e', 'i', 'o', 'u' };
+        char[] Upper_Vowel = { 'A', 'E', 'I', 'O', 'U' };
+        char[] Upper_Not_Vowel = { 'B', 'C', 'D', 'f', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'Y', 'V', 'X', 'Z', 'W' };
+        char[] Sentence_Ends = { '.', '!', '?' };
+        char[] Space_Between_Words = { ' ' };
+        public List<int> analyseText(string input)
+        {
+            //List of integers to hold the first five measurements:
+            //1. Number of sentences
+            int Sentence_Count = 0;
+            //2. Number of vowels
+
+            int Is_Vowel_Count = 0;
+            //3. Number of consonants
+            int Not_Vowel_Count = 0;
+
+            //4. Number of upper case letters
+            int Upper_Count = 0;
+            //5. Number of lower ca4se letters
+            int Lower_count = 0;
+            int Total_Letters = 0;
+            int space_words = 0;
+            bool exist = false;
+            List<int> values = new List<int>();
+            //Initialise all the values in the list to '0'
+
+
+            foreach (char ch in input)
+            {
+                if (Upper_Not_Vowel.Contains(ch))
+                {
+                    Upper_Count++;
+                    Not_Vowel_Count++;
+                    exist = true;    
+                }
+            }
+
+            foreach (char ch in input)
+            {
+                if (Upper_Vowel.Contains(ch))
+                {
+                    Upper_Count++;
+                    Is_Vowel_Count++;
+                    exist = true; 
+                }
+            }
+
+            foreach (char ch in input)
+            {
+                if (Is_VowelLower.Contains(ch))
+                {
+                    Lower_count++;
+                    Is_Vowel_Count++;
+                    exist = true;
+                }
+            }
+            
+
+            foreach (char ch in input)
+            {
+                if(Not_VowelLower.Contains(ch))
+                {
+                    Not_Vowel_Count++;
+                    Lower_count++;
+                    exist = true;
+                }
+            }
+           
+
+            foreach (char ch in input)
+            {
+                if(Sentence_Ends.Contains(ch))
+                {
+                    Sentence_Count++;
+                    exist = true;
+                }
+            }
+            
+            foreach (char ch in input)
+            {
+                if(Space_Between_Words.Contains(ch))
+                {
+                    space_words++;
+                    exist = true;
+                }
+
+            }
+
+            Total_Letters = Lower_count + Upper_Count;
+
+
+
+            for (int i = 0; i < 7; i++)
+            {
+                values.Add(0);
+            }
+
+            values[0] = Lower_count;
+            values[1] = Upper_Count;
+            values[2] = Is_Vowel_Count;
+            values[3] = Not_Vowel_Count;
+            values[4] = Sentence_Count;
+            values[5] = Total_Letters;
+            values[6] = space_words;
+            return values;
+
+
+        }
+        
+    }
+}
+
---
+++ Assignment1OOP/Assignment1 OOP VS 2019/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_OOP_VS_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Local list of integers to hold the first five measurements of the text
+            List<int> parameters = new List<int>();
+
+
+            //Create 'Input' object
+            //Get either manually entered text, or text from a file
+
+
+            Report Output_user = new Report();
+            
+            Input input1 = new Input();
+            string inputChoice = input1.ChoiceOfMethod();
+            string text = "";
+            inputChoice.Contains("Manual");
+            if (inputChoice.ToLower().Equals( "m" ) || (inputChoice.ToLower().Equals( "manual")))
+            {
+                text = input1.manualTextInput();
+                
+            }
+            else if (inputChoice.ToLower().Equals( "f" ) || (inputChoice.ToLower().Equals( "file")))
+            {
+                text = input1.fileTextInput("");
+            }
+            else
+            {
+                Console.WriteLine("INVALID INPUT, PLEASE RE-ENTER YOUR INPUT.");
+            }
+
+            //Create an 'Analyse' object
+            //Pass the text input to the 'analyseText' method
+         
+            Analyse analyse1 = new Analyse();
+            var analyse = analyse1.analyseText(text);
+            Output_user.UserOutput(analyse);
+            //Receive a list of integers back
+
+
+
+
+
+
+            //TO ADD: Get the frequency of individual letters?
+        }
+    }
+}
